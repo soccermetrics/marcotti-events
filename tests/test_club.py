245d178ea9cdde06c98a7c88245dfbae1eb55808
Howard Hamilton <hhamilton@soccermetrics.net>
2015-12-16 02:22:40 -0500

    Add goal event and modifier tests

--- tests/test_club.py
+++ tests/test_club.py
@@ -263,14 +263,14 @@ def test_club_goal_event_view(session, club_match_lineup):
             type=enums.ActionType.start_period
         ),
         mce.MatchActions(
-            event=mc.ClubMatchEvents(match_id=match.id, period=1, period_secs=1143, x=67.3, y=62.1),
+            event=mc.ClubMatchEvents(match_id=match.id, team_id=club.id, period=1, period_secs=1143, x=67.3, y=62.1),
             type=enums.ActionType.ball_pass,
             lineup_id=lineups[0].id,
             x_end=82.2,
             y_end=50.0
         ),
         mce.MatchActions(
-            event=mc.ClubMatchEvents(match_id=match.id, period=1, period_secs=1150, x=84.0, y=48.8),
+            event=mc.ClubMatchEvents(match_id=match.id, team_id=club.id, period=1, period_secs=1150, x=84.0, y=48.8),
             type=enums.ActionType.goal,
             lineup_id=lineups[1].id,
             x_end=100.0,
@@ -283,6 +283,59 @@ def test_club_goal_event_view(session, club_match_lineup):
 
     goal_events = session.query(mc.ClubGoals)
     assert goal_events.count() == 1
+    assert goal_events[0].match_id == match.id
+    assert goal_events[0].lineup_id == lineups[1].id
+    assert goal_events[0].team_id == lineups[1].team_id
+    assert goal_events[0].period == 1
+    assert goal_events[0].period_secs == 1150
+    assert goal_events[0].x == 84.0
+    assert goal_events[0].y == 48.8
+
+
+@club_only
+def test_club_goal_modifier_insert(session, club_match_lineup, modifiers):
+    club, match, lineups = club_match_lineup
+
+    modifier_objs = [mce.Modifiers(type=enums.ModifierType.from_string(record['modifier']),
+                                   category=enums.ModifierCategoryType.from_string(record['category']))
+                     for record in modifiers]
+    session.add_all(modifier_objs)
+    session.commit()
+
+    goal_action = mce.MatchActions(
+            event=mc.ClubMatchEvents(match_id=match.id, team_id=club.id, period=1, period_secs=1150, x=84.0, y=48.8),
+            type=enums.ActionType.goal,
+            lineup_id=lineups[1].id,
+            x_end=100.0,
+            y_end=45.0,
+            z_end=10.0
+    )
+    session.add(goal_action)
+    session.commit()
+
+    goal_modifiers = [
+        mce.MatchActionModifiers(
+            action_id=goal_action.id,
+            modifier_id=session.query(mce.Modifiers.id).filter_by(type=enums.ModifierType.head).scalar()
+        ),
+        mce.MatchActionModifiers(
+            action_id=goal_action.id,
+            modifier_id=session.query(mce.Modifiers.id).filter_by(type=enums.ModifierType.center_penalty_area).scalar()
+        ),
+        mce.MatchActionModifiers(
+            action_id=goal_action.id,
+            modifier_id=session.query(mce.Modifiers.id).filter_by(type=enums.ModifierType.lower_right).scalar()
+        )
+    ]
+    session.add_all(goal_modifiers)
+    session.commit()
+
+    action_from_db = session.query(mc.ClubGoals).filter_by(action_id=goal_action.id).one()
+    goal_body_part = session.query(mce.Modifiers).join(mce.MatchActionModifiers)\
+        .filter(mce.MatchActionModifiers.action_id == action_from_db.action_id,
+                mce.Modifiers.category == enums.ModifierCategoryType.bodypart)
+    assert goal_body_part.count() == 1
+    assert goal_body_part[0].type.value == "Head"
 
 
 @club_only

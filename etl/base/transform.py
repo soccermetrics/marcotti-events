8d1bbe7a356468d067b57c800a419e9129c79eef
Howard Hamilton <hhamilton@soccermetrics.net>
2016-11-02 01:32:13 -0400

    Update transformation methods for match information, events, and event modifiers using mapper models

--- etl/base/transform.py
+++ etl/base/transform.py
@@ -2,12 +2,10 @@ import pandas as pd
 
 from models.common.enums import (ConfederationType, ActionType, ModifierType,
                                  ModifierCategoryType, NameOrderType, PositionType, SurfaceType)
-from models.common.suppliers import (MatchEventMap, MatchMap, CompetitionMap, SeasonMap,
+from models.common.suppliers import (MatchEventMap, MatchMap, CompetitionMap,
                                      VenueMap, PositionMap, PlayerMap, ManagerMap, RefereeMap)
 from models.common.overview import Countries, Timezones, Competitions, Seasons, Venues, Surfaces
-from models.common.personnel import Players, Managers, Referees, Positions
-from models.common.match import MatchLineups
-from models.common.events import Modifiers
+from models.common.personnel import Players, Managers, Referees
 from models.club import Clubs, ClubLeagueMatches, ClubMap
 from .workflows import WorkflowBase
 
@@ -175,29 +173,32 @@ class MarcottiEventTransform(MarcottiTransform):
     def league_matches(self, data_frame):
         lambdafunc = lambda x: pd.Series([
             self.get_id(CompetitionMap, remote_id=x['remote_competition_id'], supplier_id=self.supplier_id),
-            self.get_id(SeasonMap, remote_id=x['remote_season_id'], supplier_id=self.supplier_id),
+            self.get_id(Seasons, name=x['season_name']),
             self.get_id(VenueMap, remote_id=x['remote_venue_id'], supplier_id=self.supplier_id),
-            self.get_id(MatchMap, remote_id=x['remote_match_id'], supplier_id=self.supplier_id),
             self.get_id(ClubMap, remote_id=x['remote_home_team_id'], supplier_id=self.supplier_id),
             self.get_id(ClubMap, remote_id=x['remote_away_team_id'], supplier_id=self.supplier_id),
-            self.get_id(ManagerMap, full_name=x['remote_home_manager_id'], supplier_id=self.supplier_id),
-            self.get_id(ManagerMap, full_name=x['remote_away_manager_id'], supplier_id=self.supplier_id),
-            self.get_id(RefereeMap, full_name=x['remote_referee_id'], supplier_id=self.supplier_id)
+            self.get_id(ManagerMap, remote_id=x['remote_home_manager_id'], supplier_id=self.supplier_id),
+            self.get_id(ManagerMap, remote_id=x['remote_away_manager_id'], supplier_id=self.supplier_id),
+            self.get_id(RefereeMap, remote_id=x['remote_referee_id'], supplier_id=self.supplier_id),
+            self.make_date_object(x['date'])
         ])
         ids_frame = data_frame.apply(lambdafunc, axis=1)
-        ids_frame.columns = ['competition_id', 'season_id', 'venue_id', 'match_id', 'home_team_id',
-                             'away_team_id', 'home_manager_id', 'away_manager_id', 'referee_id']
-        return data_frame.join(ids_frame)
+        ids_frame.columns = ['competition_id', 'season_id', 'venue_id', 'home_team_id', 'away_team_id',
+                             'home_manager_id', 'away_manager_id', 'referee_id', 'match_date']
+        joined_frame = data_frame.join(ids_frame).drop(['season_name', 'date'], axis=1)
+        return joined_frame
 
-    def lineups(self, data_frame):
+    def match_lineups(self, data_frame):
         lambdafunc = lambda x: pd.Series([
             self.get_id(MatchMap, remote_id=x['remote_match_id'], supplier_id=self.supplier_id),
             self.get_id(PlayerMap, remote_id=x['remote_player_id'], supplier_id=self.supplier_id),
-            self.get_id(ClubMap, remote_id=x['remote_team_id'], supplier_id=self.supplier_id)
+            self.get_id(ClubMap, remote_id=x['remote_team_id'], supplier_id=self.supplier_id),
+            self.get_id(PositionMap, remote_id=x['remote_position_id'], supplier_id=self.supplier_id)
         ])
         ids_frame = data_frame.apply(lambdafunc, axis=1)
-        ids_frame.columns = ['match_id', 'player_id', 'team_id']
-        return data_frame.join(ids_frame)
+        ids_frame.columns = ['match_id', 'player_id', 'team_id', 'position_id']
+        return data_frame.join(ids_frame).drop(['remote_match_id', 'remote_player_id', 'remote_team_id',
+                                                'remote_position_id'], axis=1)
 
     def events(self, data_frame):
         lambdafunc = lambda x: pd.Series([
@@ -206,18 +207,16 @@ class MarcottiEventTransform(MarcottiTransform):
         ])
         ids_frame = data_frame.apply(lambdafunc, axis=1)
         ids_frame.columns = ['match_id', 'team_id']
-        return data_frame.join(ids_frame)
+        return data_frame.join(ids_frame).drop(['remote_match_id', 'remote_team_id'], axis=1)
 
     def actions(self, data_frame):
         lambdafunc = lambda x: pd.Series([
             self.get_id(MatchEventMap, remote_id=x['remote_event_id'], supplier_id=self.supplier_id),
-            self.get_id(MatchLineups,
-                        match_id=self.get_id(MatchMap, remote_id=x['remote_match_id'], supplier_id=self.supplier_id),
-                        player_id=self.get_id(PlayerMap, remote_id=x['remote_player_id'],
-                                              supplier_id=self.supplier_id)),
+            self.get_id(MatchMap, remote_id=x['remote_match_id'], supplier_id=self.supplier_id),
+            self.get_id(PlayerMap, remote_id=x['remote_player_id'], supplier_id=self.supplier_id),
             ActionType.from_string(x['action_type']),
-            self.get_id(Modifiers, type=ModifierType.from_string(x['modifier_type']))
         ])
         ids_frame = data_frame.apply(lambdafunc, axis=1)
-        ids_frame.columns = ['match_id', 'lineup_id', 'type', 'modifier_id']
-        return data_frame.join(ids_frame)
+        ids_frame.columns = ['event_id', 'match_id', 'player_id', 'type']
+        return data_frame.join(ids_frame).drop(['remote_event_id', 'remote_match_id', 'remote_player_id',
+                                                'action_type'], axis=1)
